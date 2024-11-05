1927e75810be5f95a3eb2ee57a9fa768ff67440b
Nievas2 <angelgabrielnievas@gmail.com>
2024-11-05 11:49:00 -0300

    feat(Home): :lipstick: Add cupcake icon

--- app/(components)/Questions.tsx
+++ app/(components)/Questions.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
+import { Icon } from "@iconify/react/dist/iconify.js"
 
 const Questions = () => {
   return (
@@ -7,9 +8,15 @@ const Questions = () => {
         <h1 className="text-4xl font-bold mb-6 text-center">
           Preguntas Frecuentes
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4">
           <Card className="overflow-hidden md:col-span-1 lg:col-span-2 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">¿COMO COMPRAR?</h2>
               <p className="text-sm text-muted-foreground">
                 1. Navega por nuestra web y mira todas las opciones que tenemos
@@ -28,7 +35,13 @@ const Questions = () => {
             </CardContent>
           </Card>
           <Card className="overflow-hidden md:col-span-1 lg:col-span-1 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">
                 ¿HAY QUE SEÑAR LOS PEDIDOS?
               </h2>
@@ -42,7 +55,13 @@ const Questions = () => {
           </Card>
 
           <Card className="overflow-hidden md:col-span-1 lg:col-span-1 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">
                 LOS PRECIOS ESTAN ACTUALIZADOS?
               </h2>
@@ -53,7 +72,13 @@ const Questions = () => {
             </CardContent>
           </Card>
           <Card className="overflow-hidden md:col-span-1 lg:col-span-2 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">
                 ¿CON CUANTA ANTICIPACION DEBO HACER MI PEDIDO?
               </h2>
@@ -66,7 +91,13 @@ const Questions = () => {
           </Card>
 
           <Card className="overflow-hidden md:col-span-1 lg:col-span-2 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">
                 ¿REALIZAN PRODUCTOS SIN TACC, VEGANOS O VEGETARIANOS?
               </h2>
@@ -81,7 +112,13 @@ const Questions = () => {
             </CardContent>
           </Card>
           <Card className="overflow-hidden md:col-span-1 lg:col-span-1 bg-main shadow-md">
-            <CardContent className="p-6">
+            <CardContent className="p-6 relative">
+              <Icon
+                className="absolute top-1 right-1 text-red-main"
+                icon="game-icons:cupcake"
+                width="24"
+                height="24"
+              />
               <h2 className="text-lg font-semibold mb-2">¿REALIZAN PEDIDOS?</h2>
               <p className="text-sm text-muted-foreground">
                 Si, realizamos entregas a domicilio con costo adicional, si nos
