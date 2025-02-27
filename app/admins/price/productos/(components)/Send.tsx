04248feb46e1f35bf50d72e8b38f77a7605228f8
Nievas2 <angelgabrielnievas@gmail.com>
2025-02-27 14:08:02 -0300

    fix(prices): :bug: Product price accept null

--- app/admins/price/productos/(components)/Send.tsx
+++ app/admins/price/productos/(components)/Send.tsx
@@ -9,7 +9,7 @@ export default function Send({
   defaultValue
 }: {
   product: number
-  defaultValue: number
+  defaultValue: number | string | null
 }) {
   const [finished, setFinished] = useState(false)
 
@@ -22,7 +22,8 @@ export default function Send({
         const token = Cookies.get("token")
         if (!token) return
         const user = decodeJwt(token)
-
+        if(values.price?.toString().length == 0) values.price = null
+        
         const response = await fetch("/api/updatePrice", {
           method: "PUT",
           headers: {
@@ -55,7 +56,7 @@ export default function Send({
           className={`peer block w-full rounded-md border ${
             finished ? "border-green-600 bg-green-200" : "border-gray-200"
           } py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500`}
-          defaultValue={defaultValue}
+          defaultValue={defaultValue || ""}
           onChange={(e) => formik.setFieldValue("price", e.target.value)}
         />
         <Button variant="secondary" type="submit">Enviar</Button>
